1ba8674eac32edcd8a7df0e0ba47074bde6dbc01
p2007049 <luc.brun@etu.univ-lyon1.fr>
2023-01-31 19:13:51 +0100

    suite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public static bool jouerContreIA = false;
+    public static bool jouerContreIA = true;
 
     [SerializeField]
     GameObject red, green;
@@ -43,6 +43,8 @@ public class GameManager : MonoBehaviour
     {
         jouerContreIA = false;
         SceneManager.LoadScene("Game");
+
+
     }
 
     public void JoueurVsIA()
@@ -106,5 +108,113 @@ public class GameManager : MonoBehaviour
             }
 
         }
+
+        if(jouerContreIA)
+        {
+            //If GameFinsished then return
+            if (hasGameFinished) return;
+
+            //Raycast2D
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (!hit.collider) return;
+
+            if (hit.collider.CompareTag("appui"))
+            {
+                //Check out of Bounds
+                if (hit.collider.gameObject.GetComponent<Colonne>().targetlocation.y > 350f) return;
+
+                //Spawn the GameObject
+                Vector3 spawnPos = hit.collider.gameObject.GetComponent<Colonne>().spawnLocation;
+                Vector3 targetPos = hit.collider.gameObject.GetComponent<Colonne>().targetlocation;
+                GameObject circle = Instantiate(isPlayer ? red : green);
+                circle.transform.position = spawnPos;
+                circle.GetComponent<Mouvement>().targetPostion = targetPos;
+
+                //Increase the targetLocationHeight
+                hit.collider.gameObject.GetComponent<Colonne>().targetlocation = new Vector3(targetPos.x, targetPos.y + 54f, targetPos.z);
+
+                
+                int col = myBoard.GetBestMove(myBoard, 3);
+
+                
+                myBoard.UpdateBoard(hit.collider.gameObject.GetComponent<Colonne>().col - 1, isPlayer);
+                if (myBoard.Result(isPlayer))
+                {
+                    TXT_Tour.text = (isPlayer ? "Red" : "Green") + " Wins!";
+                    hasGameFinished = true;
+                    return;
+                }
+
+                //TurnMessage
+                TXT_Tour.text = !isPlayer ? RED_MESSAGE : GREEN_MESSAGE;
+                TXT_Tour.color = !isPlayer ? RED_COLOR : GREEN_COLOR;
+
+                //Change PlayerTurn
+                isPlayer = !isPlayer;
+            }
+
+            
+            
+        }
+        
+
     }
+
+    
+    // code version alpha 
+
+
+// private void PlayPredictedMove(int col)
+// {
+//     //Check if Game is Finished
+//     if (hasGameFinished) return;
+
+//     //Get the Target and Spawn Location from the Column Component
+//     GameObject targetColumn = GetColumn(col);
+//     Vector3 spawnPos = targetColumn.GetComponent<Colonne>().spawnLocation;
+//     Vector3 targetPos = targetColumn.GetComponent<Colonne>().targetlocation;
+
+//     //Check if Out of Bounds
+//     if (targetPos.y > 350f) return;
+
+//     //Spawn the GameObject
+//     GameObject circle = Instantiate(isPlayer ? red : green);
+//     circle.transform.position = spawnPos;
+//     circle.GetComponent<Mouvement>().targetPostion = targetPos;
+
+//     //Increase the targetLocationHeight
+//     targetColumn.GetComponent<Colonne>().targetlocation = new Vector3(targetPos.x, targetPos.y + 54f, targetPos.z);
+
+//     //Update the Board
+//     myBoard.UpdateBoard(col - 1, isPlayer);
+//     if (myBoard.Result(isPlayer))
+//     {
+//         TXT_Tour.text = (isPlayer ? "Red" : "Green") + " Wins!";
+//         hasGameFinished = true;
+//         return;
+//     }
+
+//     //Change Turn Message
+//     TXT_Tour.text = !isPlayer ? RED_MESSAGE : GREEN_MESSAGE;
+//     TXT_Tour.color = !isPlayer ? RED_COLOR : GREEN_COLOR;
+
+//     //Change Player Turn
+//     isPlayer = !isPlayer;
+// }
+
+// private GameObject GetColumn(int col)
+// {
+//     GameObject[] columns = GameObject.FindGameObjectsWithTag("appui");
+//     foreach (var column in columns)
+//     {
+//         if (column.GetComponent<Colonne>().col == col)
+//         {
+//             return column;
+//         }
+//     }
+//     return null;
+// }
+
 }
